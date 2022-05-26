bd6492a43ddf3191bb6bba359acccad04d035a51
Shane Nicholson <shanescottnicholson@gmail.com>
2022-05-26 10:12:24 -0700

    Adds deeds index to friendsshow page

--- src/views/FriendShow.vue
+++ src/views/FriendShow.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
+    <img v-bind:src="friend.image_url" v-bind:alt="`No Image`" />
     <h1>{{ friend.name }}</h1>
+    <br />
+    <h2>Defining Features: {{ friend.description }}</h2>
+    <br />
+    <h2>Social Credit Score: {{ friend.score }}</h2>
+    <p></p>
+
+    <br />
+    <br />
+    <h1>Verifiable Deeds</h1>
+    <p>-------------------------------------------</p>
+    <div v-for="deed in deeds" v-bind:key="deed.id">
+      <h2>{{ deed.deed }}</h2>
+    </div>
   </div>
 </template>
 
@@ -11,16 +25,26 @@ export default {
   data: function () {
     return {
       message: "Online",
-      friend: [],
+      friend: {},
+      deeds: [],
+      switch: false,
+      button: true,
     };
   },
   created: function () {
     this.getFriends();
+    this.getDeeds();
   },
   methods: {
     getFriends: function () {
       axios.get("http://localhost:3000/friend/" + this.$route.params.id).then((response) => {
         this.friend = response.data;
+        console.log(response.data);
+      });
+    },
+    getDeeds: function () {
+      axios.get("http://localhost:3000/friend/" + this.$route.params.id + "/deeds").then((response) => {
+        this.deeds = response.data;
         console.log(response.data);
       });
     },
